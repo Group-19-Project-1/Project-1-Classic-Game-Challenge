3e6a3f35be1122a9d6a9d686475b2e43ef999b94
cgrave314 <59934219+cgrave314@users.noreply.github.com>
2021-02-04 18:49:34 -0500

    PlayerDeath+CameraFix

--- PacManClone/Assets/Scripts/PacdotBehavior.cs
+++ PacManClone/Assets/Scripts/PacdotBehavior.cs
@@ -5,13 +5,9 @@ using UnityEngine.UI;
 
 public class PacdotBehavior : MonoBehaviour
 {
-    public GameObject scoreText;
-    public int theScore;
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.name == "Player")
-            theScore += 100;
-            scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
             Destroy(gameObject);
     }
 }
--- PacManClone/Assets/Scripts/Player.cs
+++ PacManClone/Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     public float speed = 3.0f;
 
+    public Text countText;
+    public int count;
+
     Rigidbody2D rigidbody2d;
 
     float horizontal;
@@ -16,6 +19,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
     }
     // Update is called once per frame
     void Update()
@@ -34,5 +39,19 @@ public class Player : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count +1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
