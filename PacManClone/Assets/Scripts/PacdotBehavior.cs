dcf8088ca4080e11a546fac3f4619371b9cded5e
cgrave314 <59934219+cgrave314@users.noreply.github.com>
2021-02-04 02:11:12 -0500

    try this again

--- PacManClone/Assets/Scripts/PacdotBehavior.cs
+++ PacManClone/Assets/Scripts/PacdotBehavior.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PacdotBehavior : MonoBehaviour
 {
-
+    public GameObject scoreText;
+    public int theScore;
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.name == "Player")
+            theScore += 100;
+            scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
             Destroy(gameObject);
     }
 }
