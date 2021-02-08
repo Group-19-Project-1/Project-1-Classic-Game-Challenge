c0be24bd87906cf5937c94b199b90e32d847e459
cgrave314 <59934219+cgrave314@users.noreply.github.com>
2021-02-08 16:36:20 -0500

    Added Music+StepFx

---
+++ PacManClone/Assets/Scripts/SoundTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTracker : MonoBehaviour
+{
+    public AudioSource[] soundFX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            soundFX[0].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            soundFX[0].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            soundFX[0].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            soundFX[0].Play();
+        }
+    }
+}
